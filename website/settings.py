5effa96218d7687216a591aeda93aa96c6b20d86
vaibhavpharate <62934219+vaibhavpharate@users.noreply.github.com>
2023-10-20 16:04:27 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -24,9 +24,9 @@ AUTH_USER_MODEL = 'weatherapp.Clients'
 SECRET_KEY = 'django-insecure-$e9a39+&v83kjvhe_=o4wgd)vd_8t&z89fl2(vg%7ruvx(baj!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["54.81.41.165","localhost",'EC2_DNS_NAME','127.0.0.1','www.skycaster.live','nash.ns.cloudflare.com','destiny.ns.cloudflare.com','https://www.skycaster.in']
+ALLOWED_HOSTS = ["54.81.41.165","localhost",'EC2_DNS_NAME','127.0.0.1','www.skycaster.live','nash.ns.cloudflare.com','destiny.ns.cloudflare.com','https://www.skycaster.in','https://skycaster.live']
 CSRF_TRUSTED_ORIGINS = ['https://nash.ns.cloudflare.com','https://destiny.ns.cloudflare.com','https://www.skycaster.live','https://skycaster.live']
 # Application definition
 
